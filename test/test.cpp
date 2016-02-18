37bdb588141b7debed3574eadb8b3249900050ac
jleung51 <jleung513@gmail.com>
2016-02-18 15:08:17 -0800

    Adding testing files.

---
+++ test/test.cpp
@@ -0,0 +1,15 @@
+/*
+ *
+ * Author: Jeffrey Leung
+ * Last edited: 2016-02-18
+ *
+ * This C++ file tests the functions of the exception logger Exceptional.
+ *
+ */
+
+#include "../include/exceptional.hpp"
+
+int main()
+{
+  return 0;
+}
