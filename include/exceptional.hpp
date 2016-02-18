4103ab2857b2bc93936682dc05edad7a588df6c9
jleung51 <jleung513@gmail.com>
2016-02-18 15:07:54 -0800

    Adding basic exceptional.cpp and hpp files.

---
+++ include/exceptional.hpp
@@ -0,0 +1,43 @@
+/*
+ *
+ * Author: Jeffrey Leung
+ * Last edited: 2016-02-18
+ *
+ * This C++ header file contains the function prototypes for the
+ * exception logger Exceptional.
+ *
+ */
+
+#pragma once
+
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+#include <string>
+
+namespace exceptional
+{
+
+class Logger
+{
+  public:
+    // Constructor
+    Logger();
+
+    // Destructor
+    ~Logger();
+
+  private:
+    const std::string log_filename = "log.txt";
+
+    // This method logs a thrown value as a warning.
+    template <class T>
+    void LogWarning( const T& except );
+
+    // This method logs a thrown value as an error.
+    template <class T>
+    void LogError( const T& except );
+
+};
+
+}  // End of namespace exceptional
---
+++ src/exceptional.cpp
@@ -0,0 +1,17 @@
+/*
+ *
+ * Author: Jeffrey Leung
+ * Last edited: 2016-02-18
+ *
+ * This C++ file contains the function implemenations for the
+ * exception logger Exceptional.
+ *
+ */
+
+#include <iostream>
+
+#include "../include/exceptional.hpp"
+
+namespace exceptional
+{
+}
