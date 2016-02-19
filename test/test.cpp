d433b16e71811947be4a82357efa2fd28d5398fd
jleung51 <jleung513@gmail.com>
2016-02-18 16:18:57 -0800

    Adding basic tests with different types for the logger.

--- test/test.cpp
+++ test/test.cpp
@@ -7,9 +7,41 @@
  *
  */
 
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+
 #include "../include/exceptional.hpp"
+
+exceptional::Logger log;
+
+namespace
+{
+
+// This function throws an std::out_of_range error.
+void out_of_range()
+{
+  throw std::out_of_range("Exception message for std::out_of_range.");
+}
+
+}  // End of unnamed namespace (for local functions)
 
 int main()
 {
+  int i = 948;
+  log.LogWarning(i);
+
+  std::string s = "Example string here.";
+  log.LogWarning(s);
+
+  try
+  {
+    out_of_range();
+  }
+  catch( const std::exception& e )
+  {
+    log.LogWarning(e);
+  }
+
   return 0;
 }
