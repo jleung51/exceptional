f5b4c7c9b9ed4efd8bd3d0e3613aa5224fd3d42e
jleung51 <jleung513@gmail.com>
2016-02-18 16:08:53 -0800

    Adding individual private methods for exception information; adding SeverityLevel enum class.

--- include/exceptional.hpp
+++ include/exceptional.hpp
@@ -43,6 +43,26 @@ class Logger
     const std::string log_filename_ = "log.txt";
     std::ofstream log_stream_;
 
+    enum class SeverityLevel
+    {
+      kWarning,
+      kError
+    };
+
+    // This private method logs the severity level of the exception.
+    void LogSeverityLevel( SeverityLevel sl );
+
+    // This private method logs the type of a thrown exception.
+    template <class T>
+    void LogExceptionType( const T& except );
+
+    // This private method logs the value of a thrown exception.
+    template <class T>
+    void LogExceptionValue( const T& except );
+
+    // This private method logs the message of an exception from std.
+    void LogExceptionMessage( const std::exception& except );
+
 };
 
 }  // End of namespace exceptional
