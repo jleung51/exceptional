cba290dfef63b766d5ff26e58cf28d9fa5eff9d1
jleung51 <jleung513@gmail.com>
2016-03-04 13:08:45 -0800

    Removing namespace indentation

--- src/exceptional.cpp
+++ src/exceptional.cpp
@@ -36,100 +36,101 @@ std::string GetTime()
 
 namespace exceptional
 {
-  // Constructor
-  Logger::Logger()
-  {
-    // Opens the file in append mode
-    log_stream_.open( log_filename_, std::ios::app );
 
+// Constructor
+Logger::Logger()
+{
+  // Opens the file in append mode
+  log_stream_.open( log_filename_, std::ios::app );
+
+  log_stream_
+    << "________________________________________"
+    << std::endl
+    << std::endl
+    << "LOG CREATED AT "
+    << GetTime()
+    << std::endl
+    << std::endl;
+}
+
+// Destructor
+Logger::~Logger()
+{
+  log_stream_
+    << "Warnings logged: "
+    << warning_count_
+    << std::endl
+    << "Errors logged:   "
+    << error_count_
+    << std::endl
+    << std::endl;
+
+  log_stream_.close();
+}
+
+// This public method logs a thrown std::exception as a warning.
+void Logger::LogWarning( const std::exception& except )
+{
+  ++warning_count_;
+  LogSeverityLevel( SeverityLevel::kWarning );
+  LogTime();
+  LogExceptionType( except );
+  LogExceptionMessage( except );
+  log_stream_ << std::endl;
+}
+
+// This public method logs a thrown std::exception as an error.
+void Logger::LogError( const std::exception& except )
+{
+  ++error_count_;
+  LogSeverityLevel( SeverityLevel::kError );
+  LogTime();
+  LogExceptionType( except );
+  LogExceptionMessage( except );
+  log_stream_ << std::endl;
+}
+
+// This private method logs the severity level of the exception.
+void Logger::LogSeverityLevel( SeverityLevel sl )
+{
+  std::string output;
+  switch( sl )
+  {
+    case SeverityLevel::kWarning:
+      output = "Warning:";
+      break;
+    case SeverityLevel::kError:
+      output = "Error:";
+      break;
+  }
+
+  log_stream_
+    << output
+    << std::endl;
+}
+
+// This private method logs the time of a thrown exception.
+void Logger::LogTime()
+{
+  log_stream_
+    << "Logged at: "
+    << GetTime()
+    << std::endl;
+}
+
+// This private method logs the message of an exception from std.
+void Logger::LogExceptionMessage( const std::exception& except )
+{
+  log_stream_
+    << "Exception message: "
+    << except.what();
+
+  std::string s = except.what();
+  if( s[s.length()-1] != '\n' )
+  {
     log_stream_
-      << "________________________________________"
-      << std::endl
-      << std::endl
-      << "LOG CREATED AT "
-      << GetTime()
-      << std::endl
       << std::endl;
   }
-
-  // Destructor
-  Logger::~Logger()
-  {
-    log_stream_
-      << "Warnings logged: "
-      << warning_count_
-      << std::endl
-      << "Errors logged:   "
-      << error_count_
-      << std::endl
-      << std::endl;
-
-    log_stream_.close();
-  }
-
-  // This public method logs a thrown std::exception as a warning.
-  void Logger::LogWarning( const std::exception& except )
-  {
-    ++warning_count_;
-    LogSeverityLevel( SeverityLevel::kWarning );
-    LogTime();
-    LogExceptionType( except );
-    LogExceptionMessage( except );
-    log_stream_ << std::endl;
-  }
-
-  // This public method logs a thrown std::exception as an error.
-  void Logger::LogError( const std::exception& except )
-  {
-    ++error_count_;
-    LogSeverityLevel( SeverityLevel::kError );
-    LogTime();
-    LogExceptionType( except );
-    LogExceptionMessage( except );
-    log_stream_ << std::endl;
-  }
-
-  // This private method logs the severity level of the exception.
-  void Logger::LogSeverityLevel( SeverityLevel sl )
-  {
-    std::string output;
-    switch( sl )
-    {
-      case SeverityLevel::kWarning:
-        output = "Warning:";
-        break;
-      case SeverityLevel::kError:
-        output = "Error:";
-        break;
-    }
-
-    log_stream_
-      << output
-      << std::endl;
-  }
-
-  // This private method logs the time of a thrown exception.
-  void Logger::LogTime()
-  {
-    log_stream_
-      << "Logged at: "
-      << GetTime()
-      << std::endl;
-  }
-
-  // This private method logs the message of an exception from std.
-  void Logger::LogExceptionMessage( const std::exception& except )
-  {
-    log_stream_
-      << "Exception message: "
-      << except.what();
-
-    std::string s = except.what();
-    if( s[s.length()-1] != '\n' )
-    {
-      log_stream_
-        << std::endl;
-    }
-  }
+}
 
 }  // End of namespace exceptional
