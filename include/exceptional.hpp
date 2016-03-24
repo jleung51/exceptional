7cf3ac4156cc68dd182196791b62b7d96a5262d8
jleung51 <jleung513@gmail.com>
2016-03-24 13:42:09 -0700

    Moving methods in cpp to hpp; placing logger class into local namespace
    The tester is, as of this commit, nonfunctional.
    
    In this commit, the Logger class is moved from the 'exceptional'
    namespace to the local namespace in exceptional.hpp, and all
    method definitions in exceptional.cpp are moved into exceptional.hpp
    to allow for this.
    The exceptional namespace still exists, and contains the actual
    Logger objects used for logging from other files.
    
    This is to allow the logger to be used across multiple files in a
    single project more easily and without having to redeclare a global
    Logger object in each file.

--- include/exceptional.hpp
+++ include/exceptional.hpp
@@ -1,11 +1,11 @@
 /*
  *
  * Author: Jeffrey Leung
- * Last edited: 2016-03-21
+ * Last edited: 2016-03-24
  *
- * This C++ header file contains the function prototypes for the
- * exception logger Exceptional, as well as definitions for all
- * templated methods.
+ * This C++ header file contains the definition of the exception logger
+ * class Exceptional and its methods, as well as functions which can be
+ * used to log exceptions with.
  *
  */
 
@@ -19,8 +19,33 @@
 #include <string>
 #include <typeinfo>
 
-namespace exceptional
+// For type demangling when using g++
+#ifdef __GNUG__
+#include <cxxabi.h>
+#endif
+
+namespace  // Unnamed namespace for local functions/classes
 {
+
+// Local functions:
+
+// This local function returns the current time as a string, formatted in
+// the ISO 8601 standard.
+std::string GetTime();
+
+// This local function returns the current time as a string, formatted in
+// the ISO 8601 standard.
+std::string GetTime()
+{
+  time_t raw_time;
+  time( &raw_time );
+
+  char buffer[ sizeof("1970-01-01T00:00:00") ];
+  strftime(buffer, sizeof(buffer), "%Y-%m-%dT%H:%M:%S", localtime(&raw_time));
+  return buffer;
+}
+
+// Local classes:
 
 class Logger
 {
@@ -89,6 +114,85 @@ class Logger
     void LogExceptionMessage( const std::exception& except );
 };
 
+// Default constructor
+Logger::Logger() :
+  log_stream_default_file_{},
+  log_stream_{log_stream_default_file_}
+{
+  // Opens the file in append mode
+  // Executed on the ofstream object, not the ostream object
+  log_stream_default_file_.open( log_filename_default_, std::ios::app );
+
+  log_stream_
+    << "________________________________________"
+    << std::endl
+    << std::endl
+    << "LOG CREATED AT "
+    << GetTime()
+    << std::endl
+    << std::endl;
+}
+
+// Parameterized constructor (string filename)
+Logger::Logger( const std::string filename ) :
+  log_stream_default_file_{},
+  log_stream_{log_stream_default_file_}
+{
+  // Opens the file in append mode
+  // Executed on the ofstream object, not the ostream object
+  log_stream_default_file_.open( filename, std::ios::app );
+
+  log_stream_
+    << "________________________________________"
+    << std::endl
+    << std::endl
+    << "LOG CREATED AT "
+    << GetTime()
+    << std::endl
+    << std::endl;
+}
+
+// Parameterized constructor (output stream)
+Logger::Logger( std::ostream& os ) :
+  log_stream_{os}
+{
+  log_stream_
+    << "________________________________________"
+    << std::endl
+    << std::endl
+    << "LOG CREATED AT "
+    << GetTime()
+    << std::endl
+    << std::endl;
+}
+
+// Destructor
+Logger::~Logger()
+{
+  log_stream_
+    << "Warnings logged: "
+    << warning_count_
+    << std::endl
+    << "Errors logged:   "
+    << error_count_
+    << std::endl
+    << std::endl;
+
+  // Executed on the ofstream object, not the ostream object
+  log_stream_default_file_.close();
+}
+
+// This public method logs a thrown std::exception as a warning.
+void Logger::LogWarning( const std::exception& except )
+{
+  ++warning_count_;
+  LogSeverityLevel( SeverityLevel::kWarning );
+  LogTime();
+  LogExceptionType( except );
+  LogExceptionMessage( except );
+  log_stream_ << std::endl;
+}
+
 // This public method logs a thrown value as a warning.
 template <class T>
 void Logger::LogWarning( const T& except )
@@ -98,6 +202,17 @@ void Logger::LogWarning( const T& except )
   LogTime();
   LogExceptionType( except );
   LogExceptionValue( except );
+  log_stream_ << std::endl;
+}
+
+// This public method logs a thrown std::exception as an error.
+void Logger::LogError( const std::exception& except )
+{
+  ++error_count_;
+  LogSeverityLevel( SeverityLevel::kError );
+  LogTime();
+  LogExceptionType( except );
+  LogExceptionMessage( except );
   log_stream_ << std::endl;
 }
 
@@ -111,6 +226,35 @@ void Logger::LogError( const T& except )
   LogExceptionType( except );
   LogExceptionValue( except );
   log_stream_ << std::endl;
+}
+
+
+// This private method logs the severity level of the exception.
+void Logger::LogSeverityLevel( SeverityLevel sl )
+{
+  std::string output;
+  switch( sl )
+  {
+    case SeverityLevel::kWarning:
+      output = "Warning:";
+      break;
+    case SeverityLevel::kError:
+      output = "Error:";
+      break;
+  }
+
+  log_stream_
+    << output
+    << std::endl;
+}
+
+// This private method logs the time of a thrown exception.
+void Logger::LogTime()
+{
+  log_stream_
+    << "Logged at: "
+    << GetTime()
+    << std::endl;
 }
 
 // This private method logs the type of a thrown exception.
@@ -139,6 +283,47 @@ void Logger::LogExceptionType( const T& except )
   }
 }
 
+#ifdef __GNUG__  // Using g++
+
+// This private method demangles the type of a thrown exception
+// if g++ is used.
+// An exception is thrown if:
+//   type_name is null (invalid_argument)
+std::string Logger::DemangleType( const char* type_name )
+{
+  if( type_name == nullptr )
+  {
+    throw std::invalid_argument("Error: DemangleType() was given an invalid "\
+      "(null) pointer.\n");
+  }
+
+  int status = -1;
+  char* type_name_demangled =
+    abi::__cxa_demangle( type_name, NULL, NULL, &status );
+
+  if( status == 0 )
+  {
+    std::string demangled_string( type_name_demangled );
+    free( type_name_demangled );
+    return demangled_string;
+  }
+  else
+  {
+    return type_name;
+  }
+}
+
+#else
+
+// This private method demangles the type of a thrown exception
+// if g++ is used.
+std::string Logger::DemangleType( const char* type_name )
+{
+  return type_name;
+}
+
+#endif
+
 // This private method logs the type of a thrown exception.
 template <class T>
 void Logger::LogExceptionValue( const T& except )
@@ -149,4 +334,27 @@ void Logger::LogExceptionValue( const T& except )
     << std::endl;
 }
 
-}  // End of namespace exceptional
+// This private method logs the message of an exception from std.
+void Logger::LogExceptionMessage( const std::exception& except )
+{
+  log_stream_
+    << "Exception message: "
+    << except.what();
+
+  std::string s = except.what();
+  if( s[s.length()-1] != '\n' )
+  {
+    log_stream_
+      << std::endl;
+  }
+}
+
+}  // End of unnamed namespace (local to the file)
+
+// Place loggers in here
+namespace exceptional
+{
+  Logger logger_default_file;
+  Logger logger_example_file("example_file.log");
+  Logger logger_cout(std::cout);
+}
